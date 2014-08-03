62be7215ee2be7e295f54168d92f6a9073f9ee84
sebi2306 <sebi2306@gmail.com>
2014-08-03 18:08:24 +0300

    Initial commit
    Initial commit of SebiTweaks,
    No use for Mekanism API for now

---
+++ src/api/java/cofh/api/transport/IEnderAttuned.java
@@ -0,0 +1,14 @@
+package cofh.api.transport;
+
+interface IEnderAttuned
+{
+
+    public String getChannelString();
+
+    public int getFrequency();
+
+    public boolean setFrequency(int frequency);
+
+    public boolean clearFrequency();
+
+}
---
+++ src/main/java/blocks/BlockMainClass.java
@@ -0,0 +1,43 @@
+package blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+
+import com.sebi2306.sebitweaks.Reference;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class BlockMainClass extends Block
+{
+	
+	 public BlockMainClass(Material material)
+	    {
+	        super(material);
+	    }
+
+	    public BlockMainClass()
+	    {
+	        this(Material.rock);
+	    }
+
+	    @Override
+	    public String getUnlocalizedName()
+	    {
+	        return String.format("tile.%s%s", Reference.MOD_ID.toLowerCase() + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+	    }
+
+	    @Override
+	    @SideOnly(Side.CLIENT)
+	    public void registerBlockIcons(IIconRegister iconRegister)
+	    {
+	        blockIcon = iconRegister.registerIcon(String.format("%s", getUnwrappedUnlocalizedName(this.getUnlocalizedName())));
+	    }
+
+	    protected String getUnwrappedUnlocalizedName(String unlocalizedName)
+	    {
+	        return unlocalizedName.substring(unlocalizedName.indexOf(".") + 1);
+	    }
+
+}

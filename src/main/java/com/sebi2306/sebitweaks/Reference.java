9e7cc537db2da297175153d43c9a769541872525
sebi2306 <sebi2306@gmail.com>
2014-08-03 18:16:27 +0300

    Revert "Initial commit"
    This reverts commit 62be7215ee2be7e295f54168d92f6a9073f9ee84.

--- src/main/java/com/sebi2306/sebitweaks/Reference.java
+++
@@ -1,8 +0,0 @@
-package com.sebi2306.sebitweaks;
-
-public class Reference {
-
-	public static final String MOD_ID = "SebiTweaks";
-	public static final String MOD_NAME = "SebiTweaks";
-	public static final String VERSION = "1.7.10-1.0";
-}
